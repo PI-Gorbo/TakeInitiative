b846bc142a12841b4e3f7fcf131a797bb15ff075
Gorbo <sam.i.gorbatov@gmail.com>
2024-04-10 22:53:49 +1000

    Summary Section

--- apps/TakeInitiative.Web/components/Index/SummarySection.vue
+++ apps/TakeInitiative.Web/components/Index/SummarySection.vue
@@ -1,16 +1,18 @@
 <template>
-    <main class="flex h-full flex-col items-center overflow-y-auto">
-        <div class="grid w-full flex-1 grid-cols-9 gap-4 overflow-y-auto">
+    <main
+        :class="[
+            'flex h-full flex-col overflow-y-auto',
+            !isMobile && 'items-center',
+        ]"
+    >
+        <div
+            class="grid w-full flex-1 grid-cols-9 gap-4 overflow-y-auto"
+            v-if="!isMobile"
+        >
             <section class="col-span-3 flex flex-col gap-4 overflow-y-auto">
                 <article
                     class="flex flex-1 flex-col rounded-xl border border-take-navy-medium px-2 py-1"
                 >
-                    <div class="flex-1">
-                        <div class="w-full font-NovaCut">Campaign Name</div>
-                        <div class="px-3 py-2 text-lg text-take-yellow">
-                            {{ campaign?.campaignName }}
-                        </div>
-                    </div>
                     <div class="flex-1">
                         <div class="w-full font-NovaCut">Creation Date</div>
                         <div class="px-3 py-2 text-lg text-take-yellow">
@@ -116,6 +118,99 @@
                     </div>
                 </FormBase>
             </section>
+        </div>
+        <div v-else class="flex h-full flex-col gap-2 px-2">
+            <article
+                class="flex flex-col rounded-xl border border-take-navy-medium px-2 py-1"
+            >
+                <div class="flex flex-1 flex-row items-center justify-between">
+                    <div class="font-NovaCut">Creation Date</div>
+                    <div class="px-3 py-2 text-lg text-take-yellow">
+                        {{
+                            format(
+                                parseISO(campaign?.createdTimestamp ?? ""),
+                                "PPPP",
+                            )
+                        }}
+                    </div>
+                </div>
+                <div class="flex flex-1 flex-row items-center justify-between">
+                    <div class="font-NovaCut">Last Fight Date</div>
+                    <div class="px-3 py-2 text-lg text-take-yellow">...</div>
+                </div>
+            </article>
+            <section
+                class="flex flex-col overflow-y-auto rounded-xl border border-take-navy-medium px-2 py-1"
+            >
+                <label class="block w-full font-NovaCut"> Players </label>
+                <div class="overflow-y-auto px-3 py-2">
+                    <IndexPlayersDisplay
+                        :campaignMemberDtos="campaignStore.memberDtos"
+                    />
+                </div>
+            </section>
+            <FormBase
+                class="col-span-6 flex flex-1 flex-col rounded-xl border border-take-navy-medium px-2 py-1"
+                :onSubmit="submitDetails"
+                v-slot="{ submitting }"
+            >
+                <div
+                    class="flex w-full items-center justify-between font-NovaCut"
+                >
+                    <label>Description</label>
+                    <FormButton
+                        v-if="campaignStore.isDm"
+                        type="submit"
+                        class="text-sm disabled:bg-take-navy-medium"
+                        icon="floppy-disk"
+                        :loadingDisplay="{ showSpinner: true }"
+                        :disabled="
+                            !descriptionHasChanges && !resourcesHasChanges
+                        "
+                        :isLoading="submitting"
+                    />
+                </div>
+                <div
+                    class="flex w-full flex-1 flex-col overflow-y-auto px-3 py-2"
+                >
+                    <div
+                        class="flex flex-1 flex-row gap-2 rounded-xl bg-take-navy-medium p-1"
+                    >
+                        <textarea
+                            :value="description"
+                            class="h-full w-full rounded-xl bg-take-navy-medium p-1 ring-0"
+                            @input="
+                                (e) =>
+                                    (description = (
+                                        e.target as HTMLTextAreaElement
+                                    ).value)
+                            "
+                        />
+                        <div></div>
+                    </div>
+                </div>
+                <div class="flex w-full font-NovaCut">
+                    <label>Resources</label>
+                </div>
+                <div
+                    class="flex w-full flex-1 flex-col overflow-y-auto px-3 py-2"
+                >
+                    <div
+                        class="flex flex-1 flex-row gap-2 rounded-xl bg-take-navy-medium p-1"
+                    >
+                        <textarea
+                            :value="resources"
+                            class="h-full w-full rounded-xl bg-take-navy-medium p-1 ring-0"
+                            @input="
+                                (e) =>
+                                    (resources = (
+                                        e.target as HTMLTextAreaElement
+                                    ).value)
+                            "
+                        />
+                    </div>
+                </div>
+            </FormBase>
         </div>
     </main>
 </template>
