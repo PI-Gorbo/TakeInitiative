f40401d7f4c6bf13e382fd2621bacce57dabeb2d
Gorbo <sam.I.gorbatov@gmail.com>
2024-05-13 00:38:17 +1000

    feat: :art: Reoganize into feature folders

---
+++ apps/TakeInitiative.Api/src/Features/Auth/GetAuthenticated/GetAuthenticated.cs
@@ -0,0 +1,24 @@
+using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using TakeInitiative.Api.Bootstrap;
+using TakeInitiative.Api.Models;
+
+namespace TakeInitiative.Api.Controllers;
+public class GetAuthenticated(
+    IOptions<JWTOptions> JWTOptions,
+    UserManager<ApplicationUser> UserManager,
+    SignInManager<ApplicationUser> SignInManager) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/api/authenticated");
+        AuthSchemes(CookieAuthenticationDefaults.AuthenticationScheme);
+        Policies(TakePolicies.UserExists);
+    }
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendOkAsync(ct);
+    }
+}
