06ee446e9897b230904f19ac711696b73257d938
Gorbo <sam.i.gorbatov@gmail.com>
2024-05-25 09:30:56 +1000

    hotfix: manually set the cookie

--- apps/TakeInitiative.Web/plugins/02.axios.ts
+++ apps/TakeInitiative.Web/plugins/02.axios.ts
@@ -9,31 +9,40 @@ export default defineNuxtPlugin((nuxtApp) => {
     // Register axios
     const defaultAxios: CreateAxiosDefaults = { ...axiosConfig };
     const Axios: AxiosInstance = axios.create(defaultAxios);
-    // Register to use an auth token if there is one.
 
-    console.log("Adding request interceptor...")
+    // Register to use an auth token if there is one.
     const aspNetCoreCookie = useCookie(".AspNetCore.Cookies"); // Axios does not attach the cookie on the first request. so we have to manually do it.
     Axios.interceptors.request.use((config) => {
-        console.log("Request interceptor start", aspNetCoreCookie.value)
-        console.log("Attempting with the get cookie request")
         if (aspNetCoreCookie.value) {
+            console.log("seting request cookie", aspNetCoreCookie.value)
             config.headers["Cookie"] =
                 `.AspNetCore.Cookies=${aspNetCoreCookie.value}`;
+        } else {
+            console.log("not setting the cookie")
         }
         config.withCredentials = true; // Automatically adds cookies to every request.
         return config;
     });
-    console.log("Finished Adding request interceptor")
 
     Axios.interceptors.response.use(
         (resp) => {
+            
+            console.log("In response")
+            if (resp.headers["set-cookie"]) {
+                console.log("set-cookie header present")
+              const extractedCookie = resp.headers['set-cookie'].filter(x => x.startsWith('.AspNetCore.Cookies'))[0]
+              console.log("extracted cookie", extractedCookie)
+              if (extractedCookie) {
+                console.log("Value of cookie:", extractedCookie.slice(20));
+                aspNetCoreCookie.value = extractedCookie.slice(20)  
+              }   
+            }
+
             return resp;
         },
         async (error) => {
             if (error.response.status == 401) {
                 const axiosError: AxiosError = error as AxiosError
-                console.error("Unauthenticated. Redirecting to /login")
-                console.error(axiosError?.request);
                 await navigateTo("/login");
                 throw error;
             }
