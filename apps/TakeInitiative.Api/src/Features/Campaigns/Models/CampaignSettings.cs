8c3ce6b83ec00ed9602a7c104c7d7e723d99ee7a
Gorbo <sam.I.gorbatov@gmail.com>
2024-06-13 18:04:20 +1000

    Added campaign settings

--- apps/TakeInitiative.Api/src/Features/Campaigns/Models/CampaignSettings.cs
+++ apps/TakeInitiative.Api/src/Features/Campaigns/Models/CampaignSettings.cs
@@ -13,7 +13,20 @@ public record CombatHealthDisplaySettings
     public DisplayOptions DmCharacterDisplayMethod { get; set; } = DisplayOptions.HealthyBloodied;
     public DisplayOptions OtherPlayerCharacterDisplayMethod { get; set; } = DisplayOptions.RealValue;
 }
+
+public record CombatArmourClassDisplaySettings
+{
+    public enum DisplayOptions
+    {
+        RealValue = 0,
+        Hidden = 2
+    }
+
+    public DisplayOptions DmCharacterDisplayMethod { get; set; } = DisplayOptions.Hidden;
+    public DisplayOptions OtherPlayerCharacterDisplayMethod { get; set; } = DisplayOptions.RealValue;
+}
 public record CampaignSettings
 {
     public CombatHealthDisplaySettings CombatHealthDisplaySettings { get; set; } = new();
+    public CombatArmourClassDisplaySettings CombatArmourClassDisplaySettings { get; set; } = new();
 }
