541d6001bcd0f742ac98e49e1d17c74468dbd686
Gorbo <sam.I.gorbatov@gmail.com>
2024-06-13 11:49:24 +1000

    Adjusted dockerfile

---
+++ apps/TakeInitiative.Web/layers/main/middleware/indexReroute.ts
@@ -0,0 +1,22 @@
+export default defineNuxtRouteMiddleware(async (to) => {
+
+    const userStore = useUserStore();
+    if (userStore.state.user == null) {
+        return;
+    }
+
+    // Reroute to the summary section of the first campaign in the user's list.
+    if (
+        userStore.state.user.memberCampaigns.length == 0 &&
+        userStore.state.user.dmCampaigns.length == 0
+    ) {
+        if (to.meta.requiresAuth == false) {
+            throw Error(
+                "The destination of this middleware needs to be an authorized page.",
+            );
+        }
+        return navigateTo("/createOrJoinCampaign");
+    }
+
+    return userStore.navigateToFirstAvailableCampaign();
+});
