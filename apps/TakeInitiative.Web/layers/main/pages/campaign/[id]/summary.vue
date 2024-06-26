55e22f63d9072621819883ac65c89fa3aaf68a81
Gorbo <sam.I.gorbatov@gmail.com>
2024-06-26 16:21:53 +1000

    Refactored summary page

--- apps/TakeInitiative.Web/layers/main/pages/campaign/[id]/summary.vue
+++ apps/TakeInitiative.Web/layers/main/pages/campaign/[id]/summary.vue
@@ -1,5 +1,5 @@
 <template>
-    <main :class="['flex h-full flex-col', !isMobile && 'items-center']">
+    <main class="flex flex-col">
         <header
             v-if="currentCombatInfo"
             :class="[
@@ -20,131 +20,123 @@
                 {{ combatStartedText }}
             </div>
         </header>
-        <div
-            class="grid w-full flex-1 grid-cols-9 gap-4 overflow-y-auto"
-            v-if="!isMobile"
-        >
-            <section class="col-span-3 flex flex-col gap-4 overflow-y-auto">
-                <article
-                    class="flex flex-1 flex-col rounded-xl border border-take-navy-medium px-2 py-1"
-                >
-                    <FormBase
-                        class="col-span-6 flex flex-1 flex-col"
-                        :onSubmit="submitDetails"
-                        v-slot="{ submitting }"
-                    >
-                        <div class="flex w-full justify-between font-NovaCut">
-                            <label>Description</label>
-                            <div v-if="campaignStore.isDm">
-                                <FormIconButton
-                                    icon="floppy-disk"
-                                    type="submit"
-                                    :isLoading="submitting"
-                                    :buttonColour="
-                                        descriptionHasChanges
-                                            ? 'take-yellow'
-                                            : 'take-grey-dark'
-                                    "
-                                    :class="{
-                                        disabled: !descriptionHasChanges,
-                                    }"
-                                />
-                            </div>
-                        </div>
-                        <div
-                            class="col flex w-full flex-1 flex-col overflow-y-auto px-3 py-2"
-                        >
-                            <textarea
-                                :value="description"
-                                class="w-full flex-1 resize-none rounded-md bg-take-navy p-1 text-lg text-take-yellow ring-0"
-                                :disabled="!campaignStore.isDm"
-                                :class="{
-                                    'border border-take-navy-medium':
-                                        campaignStore.isDm,
-                                }"
-                                @input="
-                                    (e) =>
-                                        (description = (
-                                            e.target as HTMLTextAreaElement
-                                        ).value)
-                                "
-                            />
-                        </div>
-                    </FormBase>
-                    <div class="flex-1">
-                        <div class="w-full font-NovaCut">Creation Date</div>
-                        <div
-                            class="px-3 py-2 text-lg text-take-yellow"
-                            v-if="campaign?.createdTimestamp"
-                        >
-                            {{
-                                format(
-                                    parseISO(campaign?.createdTimestamp ?? ""),
-                                    "PPPP",
-                                )
-                            }}
-                        </div>
-                    </div>
-                    <div class="flex-1">
-                        <div class="w-full font-NovaCut">Last Fight Date</div>
-                        <div class="px-3 py-2 text-lg text-take-yellow">
-                            ...
-                        </div>
-                    </div>
-                </article>
-                <section
-                    class="flex flex-1 flex-col overflow-y-auto rounded-xl border border-take-navy-medium px-2 py-1"
-                >
-                    <label class="block w-full font-NovaCut"> Players </label>
-                    <div class="overflow-y-auto px-3 py-2">
-                        <IndexPlayersDisplay
-                            :campaignMemberDtos="campaignStore.memberDtos"
-                        />
-                    </div>
-                </section>
+        <section class="flex flex-col gap-4 overflow-y-auto">
+            <section
+                class="flex max-h-[50%] flex-1 flex-col overflow-y-auto rounded-xl px-2 py-1"
+            >
+                <label class="block w-full font-NovaCut"> Players </label>
+                <div class="overflow-y-auto px-3 py-2">
+                    <IndexPlayersDisplay
+                        :campaignMemberDtos="campaignStore.memberDtos"
+                    />
+                </div>
             </section>
-            <section class="col-span-6 flex h-full flex-col gap-4">
+            <article class="flex flex-1 flex-col rounded-xl px-2 py-1">
                 <FormBase
-                    class="col-span-6 flex flex-1 flex-col rounded-xl border border-take-navy-medium px-2 py-1"
+                    class="col-span-6 flex flex-1 flex-col"
                     :onSubmit="submitDetails"
                     v-slot="{ submitting }"
                 >
-                    <div class="flex w-full font-NovaCut">
-                        <label>Resources</label>
-                    </div>
-                    <div
-                        class="flex w-full flex-1 flex-col overflow-y-auto px-3 py-2"
-                    >
-                        <div
-                            class="flex flex-1 flex-row gap-2 rounded-xl bg-take-navy-medium p-1"
-                        >
-                            <textarea
-                                :value="resources"
-                                class="h-full w-full rounded-xl bg-take-navy-medium p-1 ring-0"
-                                :disabled="!campaignStore.isDm"
-                                @input="
-                                    (e) =>
-                                        (resources = (
-                                            e.target as HTMLTextAreaElement
-                                        ).value)
+                    <div class="flex w-full justify-between font-NovaCut">
+                        <label>Description</label>
+                        <div v-if="campaignStore.isDm">
+                            <FormIconButton
+                                icon="floppy-disk"
+                                type="submit"
+                                :isLoading="submitting"
+                                :buttonColour="
+                                    descriptionHasChanges
+                                        ? 'take-yellow'
+                                        : 'take-grey-dark'
                                 "
+                                :class="{
+                                    disabled: !descriptionHasChanges,
+                                }"
                             />
-                            <div v-if="campaignStore.isDm">
-                                <FormButton
-                                    type="submit"
-                                    class="text-sm disabled:bg-take-navy-medium"
-                                    icon="floppy-disk"
-                                    :loadingDisplay="{ showSpinner: true }"
-                                    :disabled="!resourcesHasChanges"
-                                    :isLoading="submitting"
-                                />
-                            </div>
                         </div>
                     </div>
+                    <div
+                        class="col flex w-full flex-1 flex-col overflow-y-auto px-3 py-2"
+                    >
+                        <textarea
+                            :value="description"
+                            class="w-full flex-1 resize-none rounded-md bg-take-navy p-1 text-lg text-take-yellow ring-0"
+                            :disabled="!campaignStore.isDm"
+                            :class="{
+                                'border border-take-navy-medium':
+                                    campaignStore.isDm,
+                            }"
+                            @input="
+                                (e) =>
+                                    (description = (
+                                        e.target as HTMLTextAreaElement
+                                    ).value)
+                            "
+                        />
+                    </div>
                 </FormBase>
-            </section>
-        </div>
-        <div v-else class="flex h-full flex-col gap-2 px-2">
+                <div class="flex-1">
+                    <div class="w-full font-NovaCut">Creation Date</div>
+                    <div
+                        class="px-3 py-2 text-lg text-take-yellow"
+                        v-if="campaign?.createdTimestamp"
+                    >
+                        {{
+                            format(
+                                parseISO(campaign?.createdTimestamp ?? ""),
+                                "PPPP",
+                            )
+                        }}
+                    </div>
+                </div>
+                <div class="flex-1">
+                    <div class="w-full font-NovaCut">Last Fight Date</div>
+                    <div class="px-3 py-2 text-lg text-take-yellow">...</div>
+                </div>
+            </article>
+        </section>
+        <section class="col-span-6 flex h-full flex-col gap-4">
+            <FormBase
+                class="col-span-6 flex flex-1 flex-col rounded-xl px-2 py-1"
+                :onSubmit="submitDetails"
+                v-slot="{ submitting }"
+            >
+                <div class="flex w-full font-NovaCut">
+                    <label>Resources</label>
+                </div>
+                <div
+                    class="flex w-full flex-1 flex-col overflow-y-auto px-3 py-2"
+                >
+                    <div
+                        class="flex flex-1 flex-row gap-2 rounded-xl bg-take-navy-medium p-1"
+                    >
+                        <textarea
+                            :value="resources"
+                            class="h-full w-full rounded-xl bg-take-navy-medium p-1 ring-0"
+                            :disabled="!campaignStore.isDm"
+                            @input="
+                                (e) =>
+                                    (resources = (
+                                        e.target as HTMLTextAreaElement
+                                    ).value)
+                            "
+                        />
+                        <div v-if="campaignStore.isDm">
+                            <FormButton
+                                type="submit"
+                                class="text-sm disabled:bg-take-navy-medium"
+                                icon="floppy-disk"
+                                :loadingDisplay="{ showSpinner: true }"
+                                :disabled="!resourcesHasChanges"
+                                :isLoading="submitting"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </FormBase>
+        </section>
+        <!-- </div> -->
+        <!-- <div v-else class="flex h-full flex-col gap-2 px-2">
             <article
                 class="flex flex-col rounded-xl border border-take-navy-medium px-2 py-1"
             >
@@ -241,7 +233,7 @@
                     </div>
                 </div>
             </FormBase>
-        </div>
+        </div> -->
     </main>
 </template>
 <script setup lang="ts">
