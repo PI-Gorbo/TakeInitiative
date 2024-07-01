a13d7492986474651b6c08c5cf3a9fced16da750
Gorbo <sam.I.gorbatov@gmail.com>
2024-07-01 20:23:45 +1000

    Reset to dev's dropdown

--- apps/TakeInitiative.Web/layers/base/components/Dropdown.vue
+++ apps/TakeInitiative.Web/layers/base/components/Dropdown.vue
@@ -20,44 +20,24 @@
             <ul
                 class="z-50 flex flex-col gap-1 p-2"
                 :class="{
-                    relative: props.hoverOverContent,
+                    absolute: props.hoverOverContent,
                 }"
             >
-                <summary
+                <li
+                    v-for="item in props.items"
+                    :key="props.keyFunc(item)"
+                    @click="() => onSelectedItem(item)"
                     :class="[
+                        'rounded-md',
                         `bg-${props.colour} hover:bg-${props.hoverColour}`,
                     ]"
                 >
-                    {{
-                        props.selectedItem != null
-                            ? (props.inDropdownLabel
-                                  ? props.inDropdownLabel + " "
-                                  : "") + props.displayFunc(props.selectedItem)
-                            : labelFallback
-                    }}
-                </summary>
-                <ul
-                    class="z-[999] flex flex-col gap-1 py-2"
-                    :class="{
-                        absolute: props.hoverOverContent,
-                    }"
-                >
-                    <li
-                        v-for="item in props.items"
-                        :key="props.keyFunc(item)"
-                        @click="() => onSelectedItem(item)"
-                        :class="[
-                            'rounded-md',
-                            `bg-${props.colour} hover:bg-${props.hoverColour} border border-${elementBorderColour}`,
-                        ]"
-                    >
-                        <a>{{ props.displayFunc(item) }}</a>
-                    </li>
-                    <slot name="Footer"></slot>
-                </ul>
-            </details>
-        </li>
-    </div>
+                    <a>{{ props.displayFunc(item) }}</a>
+                </li>
+                <slot name="Footer"></slot>
+            </ul>
+        </details>
+    </li>
 </template>
 <script setup lang="ts" generic="TListItem">
 import type { TakeInitColour } from "base/utils/types/HelperTypes";
@@ -70,18 +50,15 @@ const props = withDefaults(
         displayFunc: (item: TListItem) => string;
         keyFunc: (item: TListItem) => string;
         labelFallback?: string;
-        inDropdownLabel?: string;
         headerLabel?: string;
-        colour?: TakeInitColour;
-        hoverColour?: TakeInitColour;
+        colour: TakeInitColour;
+        hoverColour: TakeInitColour;
         hoverOverContent?: boolean;
-        elementBorderColour: TakeInitColour;
     }>(),
     {
         colour: "take-navy",
         hoverColour: "take-navy-dark",
         hoverOverContent: false,
-        elementBorderColour: 'take-grey-dark'
     },
 );
 
