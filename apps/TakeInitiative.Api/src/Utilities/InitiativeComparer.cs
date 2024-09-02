1f4296c6e30fc97537b4c685a741187d28929c18
Gorbo <sam.I.gorbatov@gmail.com>
2024-09-03 09:36:45 +1000

    (wip) cleaned up commented out code

--- apps/TakeInitiative.Api/src/Utilities/InitiativeComparer.cs
+++ apps/TakeInitiative.Api/src/Utilities/InitiativeComparer.cs
@@ -4,68 +4,7 @@ namespace TakeInitiative.Utilities;
 
 public class InitiativeComparer : IComparer<DiceRoll[]>, IEqualityComparer<DiceRoll[]>
 {
-    // public int Compare(int[]? x, int[]? y)
-    // {
-    //     if (x == null && y == null)
-    //     {
-    //         return 0;
-    //     }
 
-    //     if (x == null & y != null)
-    //     {
-    //         return 1;
-    //     }
-
-    //     if (y == null && x != null)
-    //     {
-    //         return -1;
-    //     }
-
-    //     var length = Math.Min(x!.Length, y!.Length);
-    //     var currentIndex = 0;
-    //     while (x[currentIndex] == y[currentIndex] && currentIndex != length - 1)
-    //     {
-    //         currentIndex++;
-    //     }
-
-    //     return x[currentIndex] > y[currentIndex] ? 1 : -1;
-    // }
-
-    // public int Compare(DiceRoll[]? x, DiceRoll[]? y)
-    // {
-    //     throw new NotImplementedException();
-    // }
-
-    // public bool Equals(int[]? x, int[]? y)
-    // {
-    //     if (x == y) // Reference equals or null.
-    //     {
-    //         return true;
-    //     }
-
-    //     if ((x == null && y != null) || (x != null && y == null))
-    //     {
-    //         return false;
-    //     }
-
-    //     return x!.SequenceEqual(y!);
-    // }
-
-    // public bool Equals(DiceRoll[]? x, DiceRoll[]? y)
-    // {
-    //     throw new NotImplementedException();
-    // }
-
-    // public int GetHashCode([DisallowNull] int[] obj)
-    // {
-    //     return obj.Aggregate(0, (a, v) =>
-    //         HashCode.Combine(a, v.GetHashCode()));
-    // }
-
-    // public int GetHashCode([DisallowNull] DiceRoll[] obj)
-    // {
-    //     throw new NotImplementedException();
-    // }
     public int Compare(DiceRoll[]? x, DiceRoll[]? y)
     {
         if (x == null && y == null)
