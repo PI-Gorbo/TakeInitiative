2f362fac2a31df171e6e9b238baa05d1bd5e1f3c
Gorbo <sam.I.gorbatov@gmail.com>
2024-10-01 22:05:15 +1000

    Made progress on automated screenshot testing

--- apps/TakeInitiative.E2E.Tests/Integration/Features/HomePage.cs
+++ apps/TakeInitiative.E2E.Tests/Integration/Features/HomePage.cs
@@ -1,9 +1,11 @@
+using DotNet.Testcontainers.Builders;
 using Microsoft.Playwright;
 
 namespace TakeInitiative.E2E.Tests;
 
 public class HomePage : IClassFixture<AuthenticatedWebAppWithDatabaseFixture>, IAsyncLifetime
 {
+    private readonly string? csprojDirectory;
     private readonly AuthenticatedWebAppWithDatabaseFixture fixture;
     private IPlaywright playwright;
     private IBrowser browser;
@@ -11,27 +13,52 @@ public class HomePage : IClassFixture<AuthenticatedWebAppWithDatabaseFixture>, I
     public HomePage(AuthenticatedWebAppWithDatabaseFixture fixture)
     {
         this.fixture = fixture;
-        VerifyImageHash.Initialize();
-        VerifyImageHash.RegisterComparers();
+        if (!VerifyImageHash.Initialized)
+        {
+            VerifyImageHash.Initialize();
+            VerifyImageHash.RegisterComparers();
+        }
     }
 
-    [Theory]
-    [InlineData("iPhone 13")]
-    [InlineData("Desktop Chrome")]
-    public async Task CaptureHomePage(string device)
+    [Fact]
+    public async Task CaptureHomePage_Phone()
     {
-        await using var context = await browser.NewContextAsync(playwright.Devices[device]);
+        await using var context = await browser.NewContextAsync(playwright.Devices["iPhone 13"]);
+        var page = await browser.NewPageAsync(new() { 
+            IsMobile = true,
+            ViewportSize = new ViewportSize()
+            {
+                Height = 812,
+                Width = 375
+            },
+        });
+
+        await page.GotoAsync("http://127.0.0.1:3000");
+        var filePath = Path.Combine(CurrentFile.Directory(), $"HomePage-Mobile.png");
+        _ = await page.ScreenshotAsync(new()
+        {
+            Path = filePath,
+            FullPage = true
+
+        });
+        await VerifyFile(filePath);
+    }
+
+    [Fact]
+    public async Task CaptureHomePage_Desktop()
+    {
+        await using var context = await browser.NewContextAsync(playwright.Devices["Desktop Chrome"]);
         var page = await browser.NewPageAsync();
 
         await page.GotoAsync("http://127.0.0.1:3000");
-
-        var formattedDeviceName = device.Replace(" ", "-");
+        var filePath = Path.Combine(CurrentFile.Directory(), $"HomePage-Desktop.png");
         _ = await page.ScreenshotAsync(new()
         {
-            Path = $"./HomePage-{formattedDeviceName}.png",
+            Path = filePath,
             FullPage = true,
+
         });
-        await VerifyFile($"./HomePage-{formattedDeviceName}.png");
+        await VerifyFile(filePath);
     }
 
     public async Task DisposeAsync()
