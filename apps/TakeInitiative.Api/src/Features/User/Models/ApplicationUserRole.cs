1aca8149b9b691e02b9e4f828cfee84aee93ee77
Gorbo <sam.I.gorbatov@gmail.com>
2024-12-03 11:52:01 +1100

    wip: Got rid of build error.

--- apps/TakeInitiative.Api/src/Features/User/Models/ApplicationUserRole.cs
+++ apps/TakeInitiative.Api/src/Features/User/Models/ApplicationUserRole.cs
@@ -5,12 +5,6 @@ using GP.MartenIdentity;
 namespace TakeInitiative.Api.Features.Users;
 public class ApplicationUserRole : MartenIdentityRole
 {
-    public Guid Id { get; set; }
-
-    public required string? Name { get; set; }
-
-    public required string? NormalizedName { get; set; }
-
     public required IList<Claim> Claims { get; set; }
 }
 
