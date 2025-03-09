2cd636dade033cdbbcf573c25abde0da55f4a516
Gorbo <sam.I.gorbatov@gmail.com>
2025-03-09 13:48:46 +1100

    wip: Worked on landing page

--- apps/TakeInitiative.WebV2/layouts/default.vue
+++ apps/TakeInitiative.WebV2/layouts/default.vue
@@ -1,26 +1,24 @@
 
 <template>
-  <main class="flex flex-col w-full h-full">
-    <header class="border-b border-b-gold bg-secondary border-double p-2 flex justify-between">
-      <section class="flex gap-2">
-        <label class="flex items-center gap-2">
-          <img class="h-[3em] w-[3em]" src="~/assets/yellowDice.png" />
-          <label
-            class="select-none font-NovaCut text-xl text-gold md:text-3xl lg:text-2xl"
-            >Take Initiative</label
-          >
-        </label>
-      </section>
-      <section class="flex items-center px-2">
-        <a class="cursor-pointer" href="https://discord.gg/caDetpm6vk">
-          <FontAwesomeIcon :icon="faDiscord" class="text-xl text-white" />
-        </a>
-        <!-- <FormButton label="Login" @clicked="toLogin" /> -->
-      </section>
-    </header>
-    
-    <NuxtPage />
-  </main>
+    <main class="flex flex-col w-full h-full">
+        <header class="border-b border-b-gold bg-primary/5 border-double p-2 flex justify-between">
+            <section class="flex gap-2">
+                <label class="flex items-center gap-2">
+                    <img class="h-[3em] w-[3em]" src="~/assets/yellowDice.png" />
+                    <label class="select-none font-NovaCut text-xl text-gold md:text-3xl lg:text-2xl">Take
+                        Initiative</label>
+                </label>
+            </section>
+            <section class="flex items-center px-2">
+                <a class="cursor-pointer" href="https://discord.gg/caDetpm6vk">
+                    <FontAwesomeIcon :icon="faDiscord" class="text-xl text-white" />
+                </a>
+                <!-- <FormButton label="Login" @clicked="toLogin" /> -->
+            </section>
+        </header>
+
+        <NuxtPage />
+    </main>
 </template>
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
--- apps/TakeInitiative.WebV2/pages/index.vue
+++ apps/TakeInitiative.WebV2/pages/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <main class="w-full h-full bg-background bg-grid">
+    <main class="flex flex-col gap-5 items-center w-full h-full bg-background bg-grid">
+        <section class="py-20 flex flex-col justify-center gap-4">
+            <h1 class="font-NovaCut text-5xl font-bold text-gold md:text-7xl">
+                Take Initiative
+            </h1>
+            <p class="max-w-lg text-center text-lg">
+                Focus on the Story, Not the Stats <br /> Make Initiative Tracking a problem of the past
+            </p>
 
-  </main>
+            <Button>
+                <span v-if="!userIsLoggedIn">Sign Up for Free</span>
+                <span v-else>Continue to your campaigns</span>
+            </Button>
+        </section>
+
+    </main>
 </template>
 
 <script setup lang="ts">
+import Button from '~/components/ui/button/Button.vue';
+
 
 </script>
 
@@ -12,20 +27,24 @@
 <style scoped>
 @keyframes scrollGrid {
     0% {
-    background-position: 0 0;
+        background-position: 0 0;
     }
+
     100% {
-    background-position: 50% 50%; /* Match grid size here */
+        background-position: 100px 100px;
+        /* Match grid size here */
     }
 }
 
 .bg-grid {
-  background-image:
-    linear-gradient(to bottom, rgba(255, 255, 255, 0.1) 1px, transparent 1px),
-    linear-gradient(to right, rgba(255, 255, 255, 0.1) 1px, transparent 1px);
-  background-size: 50px 50px; /* Adjust grid size here */
-  animation: scrollGrid 50s linear infinite; /* Adjust animation speed here */
-  background-color: #121212; /* Or your desired background color */
+    background-image:
+        linear-gradient(to bottom, hsl(var(--primary) / 0.2) 1px, transparent 1px),
+        linear-gradient(to right, hsl(var(--primary) / 0.2) 1px, transparent 1px);
+    background-size: 50px 50px;
+    /* Adjust grid size here */
+    animation: scrollGrid 6s linear infinite;
+    /* Adjust animation speed here */
+    background-color: #121212;
+    /* Or your desired background color */
 }
-
 </style>
