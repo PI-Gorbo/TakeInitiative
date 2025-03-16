59ee40c642dc8677840db5964eb8d04eeb71065b
Gorbo <sam.I.gorbatov@gmail.com>
2025-03-16 21:04:39 +1100

    wip: Worked on the resources section

--- apps/TakeInitiative.WebV2/components/Campaign/PlayerResourcesSection.vue
+++ apps/TakeInitiative.WebV2/components/Campaign/PlayerResourcesSection.vue
@@ -6,7 +6,7 @@
                 <template v-if="(props.characters ?? []).length === 0">
                     <div
                         v-if="isViewingCurrentUsersCharacters"
-                        class="flex flex-1 items-center justify-between">
+                        class="flex flex-1 gap-1 items-center justify-between">
                         <span class="text-gray-500">
                             You've not got any characters yet.
                         </span>
@@ -30,12 +30,22 @@
                             v-for="(character, index) in props.characters"
                             :key="character.id"
                             variant="outline"
-                            class="interactable shadow-solid-sm"
-                            :disabled="!isViewingCurrentUsersCharacters">
+                            :class="{
+                                'interactable shadow-solid-sm':
+                                    isViewingCurrentUsersCharacters,
+                            }"
+                            :disabled="!isViewingCurrentUsersCharacters"
+                            @click="() => {
+                                dialogState.characterClicked = character;
+                                openDialog('edit-character')
+                            }"
+                            >
                             <FontAwesomeIcon :icon="faPerson" />
                             {{ character.name }}
                         </Button>
-                        <div class="flex justify-end">
+                        <div
+                            class="flex justify-end"
+                            v-if="isViewingCurrentUsersCharacters">
                             <Button
                                 variant="outline"
                                 class="interactable shadow-solid-sm"
@@ -53,7 +63,7 @@
             <template v-if="(props.resources ?? []).length == 0">
                 <div
                     v-if="isViewingCurrentUsersCharacters"
-                    class="flex flex-1 items-center justify-between px-2">
+                    class="flex flex-1 gap-1 items-center justify-between px-2">
                     <span class="text-gray-500"
                         >You've not got any resources yet.</span
                     >
@@ -76,26 +86,29 @@
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>
-                        {{
-                            dialogState.dialogType == "create-character"
-                                ? "Create a Character"
-                                : "Share a resource"
-                        }}
+                        {{ dialogInfoMap[dialogState.dialogType].title }}
                     </DialogTitle>
                 </DialogHeader>
-                <template
-                    v-if="dialogState.dialogType == 'create-character'">
-                    <CampaignCreatePlayerCharacterForm
-                        :onSubmit="
-                            (req) =>
-                                campaignStore
-                                    .createPlayerCharacter(req)
-                                    .then(
-                                        () =>
-                                            (dialogState.dialogOpen = false)
-                                    )
-                        " />
-                </template>
+                <Transition name="fade" mode="out-in">
+                    <div
+                        :key="'create-character'"
+                        v-if="dialogState.dialogType == 'create-character'">
+                        <CampaignCreatePlayerCharacterForm
+                            :onSubmit="
+                                (req) =>
+                                    campaignStore
+                                        .createPlayerCharacter(req)
+                                        .then(
+                                            () =>
+                                                (dialogState.dialogOpen = false)
+                                        )
+                            " />
+                    </div>
+                    <div
+                        v-else-if="dialogState.dialogType === 'edit-character'">
+                        Here
+                    </div>
+                </Transition>
             </DialogContent>
         </Dialog>
     </div>
@@ -103,6 +116,7 @@
 <script setup lang="ts">
     import { faPerson, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
     import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
     import type {
         CampaignMemberResource,
         PlayerCharacter,
@@ -119,14 +133,38 @@
         characters: PlayerCharacter[];
     }>();
 
+    type DialogType =
+        | "create-character"
+        | "create-resource"
+        | "edit-character"
+        | "edit-resource";
+
+    const dialogInfoMap: Record<DialogType, { title: string }> = {
+        "create-character": {
+            title: "Create new character",
+        },
+        "create-resource": {
+            title: "Share a new resource",
+        },
+        "edit-character": {
+            title: "Edit character",
+        },
+        "edit-resource": {
+            title: "Edit resource",
+        },
+    };
+
     const dialogState = reactive<{
         dialogOpen: boolean;
-        dialogType: "create-character" | "create-resource";
+        dialogType: DialogType;
+        characterClicked: PlayerCharacter | undefined
     }>({
         dialogOpen: false,
         dialogType: "create-character",
+        characterClicked: undefined,
     });
-    function openDialog(dialogType: "create-character" | "create-resource") {
+
+    function openDialog(dialogType: DialogType) {
         dialogState.dialogType = dialogType;
         dialogState.dialogOpen = true;
     }
