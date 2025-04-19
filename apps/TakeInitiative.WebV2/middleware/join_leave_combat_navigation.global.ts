cd712a834d1b18160ed316cbf8346e951fc88922
Gorbo <sam.I.gorbatov@gmail.com>
2025-04-19 16:52:16 +1000

    wip: Removed references to use combat store

--- apps/TakeInitiative.WebV2/middleware/join_leave_combat_navigation.global.ts
+++ apps/TakeInitiative.WebV2/middleware/join_leave_combat_navigation.global.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     if (!process.client) return;
     const combatStore = useCombatStore();
     if (to?.name != "combat-id" && from?.name == "combat-id") {
-        await combatStore.leaveCombat();
+        // await combatStore.leaveCombat();
     }
 });
