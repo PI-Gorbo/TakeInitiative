b7494ae0991cba58a4482980268eb0aab7893486
Gorbo <sam.I.gorbatov@gmail.com>
2025-06-02 20:18:47 +1000

    wip: Fixed failing tests

--- apps/TakeInitiative.Api/src/Utilities/InitiativeComparer.cs
+++ apps/TakeInitiative.Api/src/Utilities/InitiativeComparer.cs
@@ -1,59 +1,67 @@
+using System.Collections;
+
 namespace TakeInitiative.Utilities;
 
 public class InitiativeComparer : IComparer<int[]>, IEqualityComparer<int[]>, IComparer<DiceRoll[]>,
-    IEqualityComparer<DiceRoll[]>
+	IEqualityComparer<DiceRoll[]>
 {
-    public int Compare(int[]? x, int[]? y)
-    {
-        if (x == null && y == null)
-        {
-            return 0;
-        }
+	public int Compare(int[]? x, int[]? y)
+	{
+		if (x == null && y == null)
+		{
+			return 0;
+		}
 
-        if (x == null & y != null)
-        {
-            return 1;
-        }
+		if (x == null & y != null)
+		{
+			return 1;
+		}
 
-        if (y == null && x != null)
-        {
-            return -1;
-        }
+		if (y == null && x != null)
+		{
+			return -1;
+		}
 
-        var length = Math.Min(x!.Length, y!.Length);
-        var currentIndex = 0;
-        while (x[currentIndex] == y[currentIndex] && currentIndex != length - 1)
-        {
-            currentIndex++;
-        }
+		var length = Math.Min(x!.Length, y!.Length);
+		var currentIndex = 0;
+		while (x[currentIndex] == y[currentIndex] && currentIndex != length - 1)
+		{
+			currentIndex++;
+		}
 
-        return x[currentIndex] > y[currentIndex] ? 1 : -1;
-    }
+		return x[currentIndex] > y[currentIndex] ? 1 : -1;
+	}
 
-    public bool Equals(int[]? x, int[]? y)
-    {
-        if (x == y) // Reference equals or null.
-        {
-            return true;
-        }
+	public bool Equals(int[]? x, int[]? y)
+	{
+		if (x == y) // Reference equals or null.
+		{
+			return true;
+		}
 
-        if ((x == null && y != null) || (x != null && y == null))
-        {
-            return false;
-        }
+		if ((x == null && y != null) || (x != null && y == null))
+		{
+			return false;
+		}
 
-        return x!.SequenceEqual(y!);
-    }
+		return x!.SequenceEqual(y!);
+	}
 
-    public int GetHashCode(int[] obj)
-    {
-        return obj.GetHashCode();
-    }
+	public int GetHashCode(int[] obj)
+	{
+		return StructuralComparisons.StructuralEqualityComparer.GetHashCode(obj);
+	}
 
-    public int Compare(DiceRoll[]? x, DiceRoll[]? y) =>
-        this.Compare(x?.Select(x => x.Total).ToArray(), y?.Select(diceRoll => diceRoll.Total).ToArray());
+	public int Compare(DiceRoll[]? x, DiceRoll[]? y)
+	{
+		var result = this.Compare(x?.Select(diceRoll => diceRoll.Total).ToArray(),
+			y?.Select(diceRoll => diceRoll.Total).ToArray());
 
-    public bool Equals(DiceRoll[]? x, DiceRoll[]? y) =>
-        this.Equals(x?.Select(x => x.Total).ToArray(), y?.Select(diceRoll => diceRoll.Total).ToArray());
-    public int GetHashCode(DiceRoll[] obj) => this.GetHashCode(obj?.Select(x => x.Total).ToArray()!);
+		return result;
+	}
+
+	public bool Equals(DiceRoll[]? x, DiceRoll[]? y) =>
+		this.Equals(x?.Select(diceRoll => diceRoll.Total).ToArray(), y?.Select(diceRoll => diceRoll.Total).ToArray());
+
+	public int GetHashCode(DiceRoll[] obj) => this.GetHashCode(obj.Select(x => x.Total).ToArray());
 }
