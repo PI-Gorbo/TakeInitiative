9c3ba531dfb7b4231f144be14abb7e52ac894749
Gorbo <sam.I.gorbatov@gmail.com>
2025-06-09 20:11:58 +1000

    wip: Updated the release script

--- Scripts/Release.ts
+++ Scripts/Release.ts
@@ -1,11 +1,19 @@
-﻿const fs = require("fs");
+﻿import { $ } from "bun";
+
+const fs = require("fs");
 const path = require("path");
 
 const args = process.argv.slice(2);
 const type = args[0]; // patch, minor, major
 
+const currentBranch = await $`git branch --show-current`
+if (currentBranch.text() != 'dev') {
+    console.error('In order to publish a new release, please ensure you are on the dev branch.')
+    process.exit(1)
+}
+
 if (!["patch", "minor", "major"].includes(type)) {
-    console.error("Usage: bun run update-version [patch|minor|major]");
+    console.error("Usage: bun run Scripts/Release.ts [patch|minor|major]");
     process.exit(1);
 }
 
@@ -37,3 +45,8 @@ packageJson.version = newVersion;
 fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), "utf8");
 
 console.log(`Version updated to ${newVersion}`);
+
+$`git add package.json`
+$`git commit -am 'Incremented package.json to version ${newVersion}'`
+$`git push`
+$`gh pr create --title 'Push version ${newVersion} to main' --body 'This pr was automatically generated.' --base main --head dev`
